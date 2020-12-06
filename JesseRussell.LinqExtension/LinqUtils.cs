28a711cd6a0d62d4adb1783eede91c88a98f0548
techClubMember <jesserussell411@gmail.com>
2020-12-06 12:56:01 -0700

    Add project files.

---
+++ JesseRussell.LinqExtension/LinqUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JesseRussell.LinqExtension
+{
+    public static class LinqUtils
+    {
+        public static IEnumerable<T> Enumerate<T>(this T item) => new MonoEnu<T>(item);
+        public static IEnumerable<Type> GetParents(this Type self) => self.BaseType.Enumerate().Concat(self.GetInterfaces());
+        public static IEnumerable<Type> GetSelfAndParents(this Type self) => self.Enumerate().Concat(self.GetParents());
+    }
+}
---
+++ JesseRussell.LinqExtension/MonoEnu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace JesseRussell.LinqExtension
+{
+    /// <summary>
+    /// An immutable enumerable struct that stores and enumerates over a single item.
+    /// </summary>
+    /// <typeparam name="T">The type of the item stored.</typeparam>
+    public struct MonoEnu<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// The item stored
+        /// </summary>
+        public readonly T Value;
+
+        public MonoEnu(T value) => Value = value;
+
+        /// <summary>
+        /// Returns an enumerator over the single item.
+        /// </summary>
+        public IEnumerator<T> GetEnumerator() { yield return Value; }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        public static implicit operator T(MonoEnu<T> self) => self.Value;
+        public static implicit operator MonoEnu<T>(T t) => new MonoEnu<T>(t);
+    }
+}
