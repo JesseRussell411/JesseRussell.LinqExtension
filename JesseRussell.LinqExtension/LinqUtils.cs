cf6ee053b53885bd22f8887d911068e27e73119b
techClubMember <jesserussell411@gmail.com>
2021-01-23 13:51:10 -0700

    Added ToHashSet() and ToSortedSet() functions.

--- JesseRussell.LinqExtension/LinqUtils.cs
+++ JesseRussell.LinqExtension/LinqUtils.cs
@@ -10,5 +10,8 @@ namespace JesseRussell.LinqExtension
         public static IEnumerable<Type> GetParents(this Type self) => self.BaseType.Enumerate().Concat(self.GetInterfaces());
         public static IEnumerable<Type> GetSelfAndParents(this Type self) => self.Enumerate().Concat(self.GetParents());
         public static ITypeArray ToTypeArray(this IEnumerable<Type> self) => new BigTypeArray(self.ToArray());
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> stream) => new HashSet<T>(stream);
+        public static SortedSet<T> ToSortedSet<T>(this IEnumerable<T> stream) => new SortedSet<T>(stream);
+        
     }
 }
