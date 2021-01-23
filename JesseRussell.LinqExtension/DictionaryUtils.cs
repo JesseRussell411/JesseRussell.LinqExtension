6ef7444787ab384f25aa60e3974e5c575f24fd26
techClubMember <jesserussell411@gmail.com>
2021-01-23 10:27:10 -0700

    Tweaked Dictionary.EnsureKey()documentation.

--- JesseRussell.LinqExtension/DictionaryUtils.cs
+++ JesseRussell.LinqExtension/DictionaryUtils.cs
@@ -11,8 +11,8 @@ namespace JesseRussell.LinqExtension
         /// </summary>
         /// <typeparam name="K">Key type</typeparam>
         /// <typeparam name="V">Value type</typeparam>
-        /// <param name="value">The value that will be inserted if and only if the given key was not found. If the given key was found, the current value will not be modified and this parameter will not be used. A default value is used if nothing is provided.</param>
-        /// <returns>True if the key was found in the dictionary, and False if the key was not found and needed to be inserted.</returns>
+        /// <param name="value">The value that will be inserted if and only if the given key was not found. If the given key was found, the current value will not be modified and this parameter will not be used.</param>
+        /// <returns>True if the key was found in the dictionary and False if the key was not found and needed to be inserted.</returns>
         public static bool EnsureKey<K, V>(this Dictionary<K, V> self, K key, V value = default)
         {
             if (self.ContainsKey(key)) return true;
