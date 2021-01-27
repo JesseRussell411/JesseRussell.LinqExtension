e2ee6d80105d43d430db1eb47e34c4fd606f37ed
techClubMember <jesserussell411@gmail.com>
2021-01-27 14:43:52 -0700

    Removed ToHashSet because it turns out that's already in linq

--- Debugging/Debugging.cs
+++ Debugging/Debugging.cs
@@ -12,6 +12,11 @@ namespace Debugging
         {
             "bob".DoOntoSelf(Console.WriteLine);
             "steven".DoOntoSelf(Console.Write);
+            Console.WriteLine(Enumerable.Range(1, 42).Select(n => $"{ 2 * n}").Aggregate((p, c) => $"{p}, {c}"));
+            var roci = Array.AsReadOnly(Enumerable.Range(1, 42).ToArray());
+            var roci2 = Enumerable.Range(1, 42).ToArray().Enumerate().Select(ia => new HashSet<int>(ia)).First();
+            var roci2 = Enumerable.Range(1, 42).ToArray().DoOntoSelf(ia => new HashSet<int>(ia));
+            Console.ReadLine();
             //Dictionary<char, int> cCount = new Dictionary<char, int>();
 
             //string s = "the quick brown fox jumps over the lazy dog";
